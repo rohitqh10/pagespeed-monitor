2c29e6317639652ba29491a387954e2ed89d615b
Rohit Gupta <rohit01.c@quickheal.com>
2026-06-29 15:40:53 +0530

    Update pagespeed_monitor.py

--- pagespeed_monitor.py
+++ pagespeed_monitor.py
@@ -19,13 +19,18 @@ start_time = time.time()
 # ---------- SETUP ----------
 options = webdriver.ChromeOptions()
 
-options.add_argument("--start-maximized")
-options.add_argument("--disable-gpu")
+# Works on GitHub Actions and locally
+options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
-options.add_argument("--disable-session-crashed-bubble")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--hide-scrollbars")
+options.add_argument("--disable-extensions")
 options.add_argument("--disable-infobars")
 options.add_argument("--no-first-run")
 options.add_argument("--no-default-browser-check")
+
 options.add_experimental_option("prefs", {
     "profile.exit_type": "Normal"
 })
